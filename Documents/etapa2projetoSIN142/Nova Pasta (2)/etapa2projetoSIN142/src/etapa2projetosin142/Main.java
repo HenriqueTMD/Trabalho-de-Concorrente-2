cbb2edbcdbc8dacfdb5268baf14b2e508c9315f1
henrique <henriquetmd0@gmail.com>
2019-12-04 19:30:48 -0300

    Trabalho Concorrente 2 Final

---
+++ Documents/etapa2projetoSIN142/Nova Pasta (2)/etapa2projetoSIN142/src/etapa2projetosin142/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package etapa2projetosin142;
+
+/**
+ *
+ * @author edunu
+ */
+public class Main {
+    
+        public static void main(String args[]){
+            frmVisualizar interface1 = new frmVisualizar();
+            interface1.setLocationRelativeTo(null);
+            interface1.setVisible(true);
+            
+        }
+}
